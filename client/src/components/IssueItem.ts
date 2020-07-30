8cc45588987dd0a8840a7e4d63d7852322df4219
Steve Jenkins <sjenkins@junglescout.com>
2020-07-30 09:54:09 -0700

    starting to build loading and delete states and some formatting

--- client/src/components/IssueItem.ts
+++ client/src/components/IssueItem.ts
@@ -12,6 +12,11 @@ export class IssueItem extends HTMLLIElement {
 
   private issue: IIssue;
 
+  private onDelete(): void {
+    this.loading = true;
+    alert('TODO delete');
+  }
+
   private render(): void {
     const template = document.querySelector('#tmpl-issue-item') as HTMLTemplateElement;
     const templateContent = template.content;
@@ -19,7 +24,34 @@ export class IssueItem extends HTMLLIElement {
     templateContent.querySelector('textarea').textContent = this.issue.title;
 
     this.appendChild(templateContent.cloneNode(true));
+
+    this.deleteButton.addEventListener('click', this.onDelete.bind(this));
   }
 
+  private get controlEl(): HTMLDivElement {
+    return this.querySelector('.control') as HTMLDivElement;
+  }
+
+  private get deleteButton(): HTMLButtonElement {
+    return this.querySelector('.delete-button') as HTMLButtonElement;
+  }
+
+  private toggleDelete(isVisible: boolean) {
+    if (isVisible) {
+      this.removeAttribute('style');
+    } else {
+      this.querySelector('.icon').setAttribute('style', 'display: none;');
+    }
+  }
+
+  public set loading(isLoading: boolean) {
+    this.toggleDelete(!isLoading);
+
+    if (isLoading) {
+      this.controlEl.classList.add('is-loading');
+    } else {
+      this.controlEl.classList.remove('is-loading');
+    }
+  }
 
 }
